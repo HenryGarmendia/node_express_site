9c563d6e873914c5b092b36291d73a0f2c524427
HenryGarmendia <me@henrygarmendia.com>
2018-03-16 14:39:40 -0500

    install nodemon for server autostart

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ let http = require('http');
 
 let myServer = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type" : "text/html"});
-    res.write('<h1>Pro Meetups</h1>'); 
+    res.write('<h1>Pro Meetup</h1>'); 
     res.end();
 });
 
