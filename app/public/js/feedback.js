01a3650bb2cb0c51f67159e722afd3a62982cc61
HenryGarmendia <me@henrygarmendia.com>
2018-03-20 23:04:59 -0500

    added click event to delete message

--- app/public/js/feedback.js
+++ app/public/js/feedback.js
@@ -8,13 +8,24 @@ $(() => {
       title: $('#feedback-form-title').val(),
       message: $('#feedback-form-message').val()
     }, updateFeedback);
-  });
+  }); // selecting the form
+
+  $('.feedback-messages').on('click', (e) => {
+    if (e.target.className = 'glyphicon glyphicon-remove') {
+      $.ajax({
+        url: 'api/' + e.target.id,
+        type: 'DELETE',
+        success: updateFeedback
+      }); // ajax
+    } // the target is a delete button
+  }); // feedback messages
 
   function updateFeedback(data) {
     let output = '';
     $.each(data, (key, item) => {
       output += '     <div class="feedback-item item-list media-list">';
       output += '       <div class="feedback-item media">';
+      output += '       <div class="media-left"><button class="feedback-delete btn btn-xs btn-danger"><span id="' + key + '" class="glyphicon glyphicon-remove"></span></button>';
       output += '         <div class="feedback-info media-body">';
       output += '           <div class="feedback-head">';
       output += '             <div class="feedback-title">' + item.title + ' <small class="feedback-name label label-info">' + item.name +'</small></div>';
@@ -25,5 +36,5 @@ $(() => {
       output += '     </div>';
     });
     $('.feedback-messages').html(output);
-  }
+  } // updateFeedback function
 });
--- app/routes/api.js
+++ app/routes/api.js
@@ -14,7 +14,19 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.post('/api', (req, res) => {
    feedbackData.unshift(req.body);
    fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', (err) => {
-      console.log(err);
+      if (err) {
+         console.log(err);
+      }
+   });
+   res.json(feedbackData);
+});
+
+router.delete('/api/:id', (req, res) => {
+   feedbackData.splice(req.params.id, 1);
+   fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', (err) => {
+      if (err) {
+         console.log(err);
+      }
    });
    res.json(feedbackData);
 });
