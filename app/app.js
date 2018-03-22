e9feecd0ae400ddd3af7de8118a9bccb02ed4390
HenryGarmendia <me@henrygarmendia.com>
2018-03-21 19:28:38 -0500

    Implemented socket.io to the project and modified of the chat message is handled

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ let express = require('express');
 let reload = require('reload');
 let app = express();
 let dataFile = require('./data/data.json');
+let io = require('socket.io')();
 
 // flexible application Node PORT
 app.set('port', process.env.PORT || 3000);
@@ -9,7 +10,7 @@ app.set('appData', dataFile);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
-app.locals.siteTitle = 'Pro Meetups';
+app.locals.siteTitle = 'Pro Meetup';
 app.locals.allContributors = dataFile.contributors;
 
 app.use(express.static('app/public'));
@@ -21,6 +22,13 @@ app.use(require('./routes/chat'));
 
 let server = app.listen(app.set('port'), () => {
     console.log('Server is listening on port ' + app.get('port'));
+});
+
+io.attach(server);
+io.on('connection', (socket) => {
+	socket.on('postMessage', (data) => {
+		io.emit('updateMessages', data);
+	});
 });
 
 reload(app);
--- app/public/js/chat.js
+++ app/public/js/chat.js
@@ -1,24 +1,35 @@
-let chatForm = document.forms.chatForm;
+let socket = io();
+let chatUsername = document.querySelector('#chat-username');
+let chatMessage = document.querySelector('#chat-message');
 
-if (chatForm) {
-   let chatUsername = document.querySelector('#chat-username');
-   let chatMessage = document.querySelector('#chat-message');
-
-   chatForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-      showMessage({
-         username: chatUsername.value,
-         message: chatMessage.value,
-      });
-      chatMessage.value = '';
-      chatMessage.focus();
-   });
-}
+socket.on('connect', () => {
+   let chatForm = document.forms.chatForm;
+   
+   if (chatForm) {
+      chatForm.addEventListener('submit', (e) => {
+         e.preventDefault();
+         socket.emit('postMessage',{
+            username: chatUsername.value,
+            message: chatMessage.value,
+         });
+         chatMessage.value = '';
+         chatMessage.focus();
+      }); // chatform event
+      socket.on('updateMessages', (data) => {
+         showMessage(data);
+      }); // updateMessages
+   } // chatform
+}); // socket
 
 function showMessage(data) {
    let chatDisplay = document.querySelector('.chat-display');
    let newMessage = document.createElement('p');
-   newMessage.className = 'bg-success chat-text';
+   if (chatUsername.value === data.username) {
+      newMessage.className = 'bg-success chat-text';
+   } else {
+      newMessage.className = 'bg-info text-warning chat-text';      
+   }
+
    newMessage.innerHTML = '<strong>' + data.username + '</strong>: ' + data.message;
    chatDisplay.insertBefore(newMessage, chatDisplay.firstChild);
-}
+} // showMessage
